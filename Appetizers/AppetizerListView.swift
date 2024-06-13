906709b8dca76c731655124b88d1a7281bdfd1db
jasonfangmagic <jasonfangmagic@gmail.com>
2024-06-13 17:00:08 -0400

    add loading views

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -13,23 +13,32 @@ struct AppetizerListView: View {
     
     
     var body: some View {
-        NavigationView{
-            List(viewModel.appetizers) {
-                appetizer in
-                AppetizerListCell(appetizer: appetizer)
+        
+        
+        ZStack {
+            
+            NavigationView{
+                List(viewModel.appetizers) {
+                    appetizer in
+                    AppetizerListCell(appetizer: appetizer)
+                }
+                .navigationTitle("🍟Appetizers")
             }
-            .navigationTitle("🍟Appetizers")
+            .onAppear{
+                viewModel.getAppetizers()
+            }
+            
+            if viewModel.isLoading {
+                LoadingView()
+            }
         }
-        .onAppear{
-            viewModel.getAppetizers()
-        }
-        .alert(item: $viewModel.alertItem) {
-            alertItem in Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            .alert(item: $viewModel.alertItem) {
+                alertItem in Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
         }
     }
     
    
-}
 
 #Preview {
     AppetizerListView()
--- Appetizers/AppetizerTabview.swift
+++ Appetizers/AppetizerTabview.swift
@@ -31,7 +31,7 @@ struct AppetizerTabview: View {
                     Text("Order")
                 }
         }
-        .accentColor(Color("brandPrimary"))
+        .accentColor(.brandPrimary)
     }
 }
 
