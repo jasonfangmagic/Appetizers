04199c0c594fab6ec7cb288b2885c22bce9c2320
jasonfangmagic <jasonfangmagic@gmail.com>
2024-06-14 16:11:00 -0400

    add x

---
+++ Appetizers/Screens/AppetizerListView/AppetizerdetailView.swift
@@ -0,0 +1,104 @@
+//
+//  AppetizerdetailView.swift
+//  Appetizers
+//
+//  Created by Jason Fang on 6/14/24.
+//
+
+import SwiftUI
+
+struct AppetizerdetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack{
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            
+            VStack{
+                Text(appetizer.name)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack (spacing: 40){
+                    VStack (spacing: 5){
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    VStack (spacing: 5){
+                        Text("Cabs")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    VStack (spacing: 5){
+                        Text("Protien")
+                            .bold()
+                            .font(.caption)
+                        
+                        Text("\(appetizer.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                print("tapped")
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 30)
+            
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button{
+            print("dismiss")
+        } label: {
+            ZStack{
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color.brandPrimary)
+            }
+        }, alignment: .topTrailing)
+    }
+}
+
+#Preview {
+    AppetizerdetailView(appetizer: MockData.sampleAppetizer)
+}
